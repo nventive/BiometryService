1ba374eacb7b44d608f41df2be11f7ca72cb7b7b
Guillaume Eniona <guillaume.eniona@nventive.com>
2021-03-04 09:04:14 -0500

    Add reference in the sample App

--- Src/BiometryService.SampleApp.Uno/BiometryService.SampleApp.Uno.Shared/MainPage.xaml.cs
+++ Src/BiometryService.SampleApp.Uno/BiometryService.SampleApp.Uno.Shared/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,57 @@ namespace BiometryService.SampleApp.Uno
 	/// </summary>
 	public sealed partial class MainPage : Page
 	{
+		private readonly IBiometryService _biometryService;
+
 		public MainPage()
 		{
 			this.InitializeComponent();
+
+			var options = new BiometryOptions();
+			options.LocalizedReasonBodyText = "REASON THAT APP WANTS TO USE BIOMETRY :)";
+			options.LocalizedFallbackButtonText = "FALLBACK :(";
+			options.LocalizedCancelButtonText = "CANCEL :'(";
+
+			// use LAPolicy.DeviceOwnerAuthenticationWithBiometrics for biometrics only with no fallback to passcode/password
+			// use LAPolicy.DeviceOwnerAuthentication for biometrics+watch with fallback to passcode/password
+			//_biometryService = new BiometryService.BiometryService(options, LAPolicy.DeviceOwnerAuthentication);
 		}
+
+		private async Task Authenticate(CancellationToken ct)
+		{
+			var capabilities = _biometryService.GetCapabilities();
+			if (!capabilities.PasscodeIsSet || !capabilities.IsSupported || !capabilities.IsEnabled)
+			{
+				//if (!capabilities.PasscodeIsSet)
+				//{
+				//	View.BackgroundColor = UIColor.Black;
+				//}
+				//else if (!capabilities.IsSupported)
+				//{
+				//	View.BackgroundColor = UIColor.Brown;
+				//}
+				//else if (!capabilities.IsEnabled)
+				//{
+				//	View.BackgroundColor = UIColor.SystemGray2Color;
+				//}
+
+				return;
+			}
+
+			var result = await _biometryService.Authenticate(ct);
+			switch (result)
+			{
+				//case BiometryAuthenticationResult.Granted:
+				//	View.BackgroundColor = UIColor.SystemGreenColor;
+				//	break;
+				//case BiometryAuthenticationResult.Denied:
+				//	View.BackgroundColor = UIColor.SystemRedColor;
+				//	break;
+				//case BiometryAuthenticationResult.Cancelled:
+				//	View.BackgroundColor = UIColor.SystemYellowColor;
+				//	break;
+			}
+		}
+
 	}
 }
